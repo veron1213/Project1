15665e79295021bdd04a1fd71914f833be4816ef
vf201 <vf201@DESKTOP-UDR3D8C>
2019-12-24 22:07:56 +0300

    Обн

--- 10/Program.cs
+++ 10/Program.cs
@@ -10,7 +10,7 @@ namespace _10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HELLO GitHub");
+            Console.WriteLine("Знакомство с GitHub");
             Console.WriteLine("Проверка изменений на сервере");
         }
     }
